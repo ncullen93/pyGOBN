3336ef7712c973b2d2c23b374e00ce519b6d36a1
ncullen93 <nicholas.c.cullen.th@dartmouth.edu>
2016-02-23 16:18:52 -0500

    setup.py added

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(name='pyGOBN',
       version='1.0',
@@ -8,13 +9,13 @@ setup(name='pyGOBN',
       author='Nicholas Cullen',
       author_email='ncullen.th@dartmouth.edu',
       url='sites.dartmouth.edu/ncullen',
-      py_modules = ['pyGOBN.pyGOBN'],
-      long_description="""
-      	Global Optimization of Bayesian Networks through
+      packages=find_packages(),
+      #py_modules = ['pyGOBN.pyGOBN'],
+      long_description="""Global Optimization of Bayesian Networks through
       	Integer Linear Programming. This module provides
       	wrapper functions for unpacking/making/installing/running
       	the GOBNILP source code. GOBNILP is a project aimed at 
       	solving the EXACT formulation of the Bayesian Network
       	structure learning problem. For further details, see
-      	the GOBNILP main website.
+      	the GOBNILP main website."""
      )
